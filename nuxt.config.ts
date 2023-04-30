690b04015fb5ac1b292d0fcbefbd4f1c0167343d
vladm1r <v13911@mail.ru>
2023-04-30 23:08:28 +0300

    feat: Добавлен element-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@/assets/scss/variables.scss']
+  css: ['@/assets/scss/variables.scss'],
+  modules: ['@element-plus/nuxt']
 })
