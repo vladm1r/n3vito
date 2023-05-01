dc1c360634e94048d748ed85e593ed4cde320f93
vladm1r <v13911@mail.ru>
2023-05-02 01:13:49 +0300

    feat: добавлен шрифт Roboto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'n3vito - объявления',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
+        }
+      ]
+    }
+  },
   modules: ['@element-plus/nuxt'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "@/assets/scss/variables.scss";'
+          additionalData: '@import "@/assets/scss/global.scss";'
         }
       }
     }
