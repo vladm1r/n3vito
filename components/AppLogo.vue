f855523b4ab5b63ab4671a2c6221f4f204a7a40e
vladm1r <v13911@mail.ru>
2023-05-02 01:22:21 +0300

    feat: Добавлен компонент Logo

---
+++ components/AppLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink to="/" class="logo">
+    n3vito
+  </NuxtLink>
+</template>
+
+<style lang="scss">
+.logo {
+  font-size: 40px;
+  text-decoration: none;
+  color: $color-text-primary;
+
+  @media(max-width: $bp-mobile) {
+    font-size: 28px;
+  }
+}
+</style>
