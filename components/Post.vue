ad7aacb7a1e9e8b05b1ef4bd17493fcbd50932a8
vladm1r <v13911@mail.ru>
2023-05-11 21:24:10 +0300

    feat: Добавлен компонет объявления

---
+++ components/Post.vue
@@ -0,0 +1,91 @@
+<template>
+  <article class="post">
+    <div class="post__header">
+      <h1 class="post__title">
+        {{ postTemplate.title }}
+      </h1>
+
+      <PriceDisplay :value="postTemplate.price" class="post__price" />
+    </div>
+
+    <div class="post__body">
+      <div>
+        <div class="post__img-container">
+          <el-image class="post__img" :src="postTemplate.image" fit="scale-down" />
+        </div>
+
+        <h3 class="post__description-title">
+          Описание
+        </h3>
+
+        <p class="post__desctiption">
+          {{ postTemplate.description }}
+        </p>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+interface Post {
+  title: string,
+  price: number,
+  image: string,
+  description: string
+}
+
+const postTemplate: Post = {
+  title: 'Супер гитара',
+  price: 10999,
+  image: '/img/guitar.jpg',
+  description: 'Купите гитару пожалуйста'
+}
+
+</script>
+
+<style lang="scss">
+.post {
+  max-width: $container-width;
+  margin: 0 auto;
+
+  &__header {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 40px;
+  }
+
+  &__title {
+    font-size: 32px;
+  }
+
+  &__price {
+    font-size: 32px;
+  }
+
+  &__body {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 40px;
+  }
+
+  &__img-container {
+    height: 400px;
+    background: #b2b2b2;
+  }
+
+  &__img {
+    width: 100%;
+    height: 100%;
+  }
+
+  &__description-title {
+    font-size: 22px;
+    margin-bottom: 10px;
+  }
+
+  &__desctiption {
+    font-size: 16px;
+  }
+}
+
+</style>
---
+++ components/PriceDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ formattedValue }}</p>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  value: number
+}>()
+
+const formattedValue = computed<string>(() => {
+  return new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2
+  }).format(props.value)
+})
+</script>
