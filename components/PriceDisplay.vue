da79a39415b991db26fc823a98276a1537da14bf
vladm1r <v13911@mail.ru>
2023-05-15 19:40:10 +0300

    feat: форматтер цены вынесен в папку Lib

--- components/PriceDisplay.vue
+++ components/PriceDisplay.vue
@@ -3,16 +3,13 @@
 </template>
 
 <script setup lang="ts">
+import { currencyFormatter } from '../Lib/currencyFormatter'
+
 const props = defineProps<{
   value: number
 }>()
 
 const formattedValue = computed<string>(() => {
-  return new Intl.NumberFormat('ru-RU', {
-    style: 'currency',
-    currency: 'RUB',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 2
-  }).format(props.value)
+  return currencyFormatter(props.value)
 })
 </script>
