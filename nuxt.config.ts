f96329688601cae85ed124a9cd4f5cf204c8f2dd
vladm1r <v13911@mail.ru>
2023-06-29 15:37:49 +0300

    feat: Добавлена ui библиотека PrimeVue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,15 @@ export default defineNuxtConfig({
       ]
     }
   },
+  css: [
+    'primevue/resources/themes/saga-blue/theme.css',
+    'primevue/resources/primevue.css',
+    'primeicons/primeicons.css',
+    'primeflex/primeflex.css'
+  ],
+  build: {
+    transpile: ['primevue']
+  },
   vite: {
     css: {
       preprocessorOptions: {
