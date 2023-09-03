e6d1a8e67248a1b31a9dd892b0b38844f76d1f0a
vladm1r <v13911@mail.ru>
2023-09-03 22:39:28 +0300

    feat: Добавлена страница создания объявления

---
+++ pages/post/create.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { useToast } from 'primevue/usetoast'
+import type { Post } from '../../types'
+
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+const toast = useToast()
+
+const post:Post = reactive({
+  title: '',
+  price: 0,
+  image_url: '',
+  description: '',
+  user_id: user.value.id
+})
+
+const createPost = async () => {
+  try {
+    const result = await supabase
+      .from('posts')
+      .insert(post)
+
+    if (!result.error) {
+      toast.add({ severity: 'success', summary: 'Успех', detail: 'Объявление создано', life: 3000 })
+    } else {
+      throw new Error(result.error.message)
+    }
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'Ошибка загрузки изображения', detail: getErrorMessage(error), life: 3000 })
+  }
+}
+
+</script>
+
+<template>
+  <FormKit
+    type="form"
+    submit-label="Создать"
+    @submit="createPost"
+  >
+    <FormKit
+      v-model="post.title"
+      type="text"
+      label="Название"
+    />
+
+    <ImageFormElement v-model="post.image_url" />
+
+    <FormKit
+      v-model="post.description"
+      type="textarea"
+      label="Описание"
+    />
+
+    <FormKit
+      v-model="post.price"
+      type="number"
+      label="Цена"
+    />
+  </FormKit>
+</template>
+
+<style lang="scss">
+
+</style>
