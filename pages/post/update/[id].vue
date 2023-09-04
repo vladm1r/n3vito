5aa07b98d3fbc51cf60e861d34ef8a07d1ecc3da
vladm1r <v13911@mail.ru>
2023-09-04 22:30:14 +0300

    feat: Добавлена страница редактирования объявления

---
+++ pages/post/update/[id].vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { useToast } from 'primevue/usetoast'
+import type { createdPost } from '@/types'
+
+const supabase = useSupabaseClient()
+const toast = useToast()
+const route = useRoute()
+
+const post:createdPost = reactive({
+  title: '',
+  price: 0,
+  image_url: '',
+  description: ''
+})
+
+const getPost = async () => {
+  try {
+    const result = await supabase
+      .from('posts')
+      .select().eq('id', route.params.id)
+
+    if (!result.error) {
+      const data = result.data[0]
+      post.title = data.title
+      post.price = data.price
+      post.image_url = data.image_url
+      post.description = data.description
+    } else {
+      throw new Error(result.error.message)
+    }
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'Ошибка при получении данных', detail: getErrorMessage(error), life: 3000 })
+  }
+}
+
+const updatePost = async () => {
+  try {
+    const result = await supabase
+      .from('posts')
+      .update(post)
+      .eq('id', route.params.id)
+
+    if (!result.error) {
+      toast.add({ severity: 'success', summary: 'Успех', detail: 'Объявление обновлено', life: 3000 })
+    } else {
+      throw new Error(result.error.message)
+    }
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'Ошибка при обновлении', detail: getErrorMessage(error), life: 3000 })
+  }
+}
+
+getPost()
+
+</script>
+
+<template>
+  <FormKit
+    type="form"
+    submit-label="обновить"
+    @submit="updatePost"
+  >
+    <FormKit
+      v-model="post.title"
+      type="text"
+      label="Название"
+    />
+
+    <ImageFormElement v-model="post.image_url" />
+
+    <FormKit
+      v-model="post.description"
+      type="textarea"
+      label="Описание"
+    />
+
+    <FormKit
+      v-model="post.price"
+      type="number"
+      label="Цена"
+    />
+  </FormKit>
+</template>
+
+<style>
+
+</style>
