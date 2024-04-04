52458dd5023527e10bdeddd83e481b343aa7d315
vladm1r <v13911@mail.ru>
2024-04-04 17:57:52 +0300

    feat: Объединены страницы формы регистрации и логина

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,15 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
   const user = useSupabaseUser()
 
-  if (!user.value && to.path === '/account') {
-    return navigateTo('/login')
-  }
-
   if (user.value && to.path === '/login') {
-    return navigateTo('/')
-  }
-
-  if (user.value && to.path === '/signup') {
     return navigateTo('/')
   }
 })
--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { ElNotification } from 'element-plus'
 
-const client = useSupabaseClient()
+const supabase = useSupabaseClient()
 
 const email = ref('')
 const password = ref('')
 
-async function onSubmit () {
+const isSignUpMode = ref(false)
+
+const submitLabel = computed(() => {
+  return isSignUpMode.value ? 'Зарегистрироваться' : 'Войти'
+})
+
+const emailPlaceholder = computed(() => {
+  return isSignUpMode.value ? 'Подтверждения не требуется :)' : 'example@mail.ru'
+})
+
+const login = async () => {
   try {
-    const response = await client.auth.signInWithPassword({
+    const response = await supabase.auth.signInWithPassword({
       email: email.value,
       password: password.value
     })
@@ -22,19 +32,46 @@ async function onSubmit () {
     ElNotification({ type: 'error', title: 'Ошибка авторизации', message: getErrorMessage(error) })
   }
 }
+
+const signup = async () => {
+  try {
+    const response = await supabase.auth.signUp({
+      email: email.value,
+      password: password.value
+    })
+
+    if (response.error) {
+      throw new Error(response.error.message)
+    } else {
+      navigateTo('/')
+    }
+  } catch (error) {
+    ElNotification({ type: 'error', title: 'Ошибка регистрации', message: getErrorMessage(error) })
+  }
+}
+
+const onSubmit = () => {
+  if (isSignUpMode) {
+    signup()
+  } else {
+    login()
+  }
+}
+
 </script>
 
 <template>
   <div class="login-form-container">
     <FormKit
       type="form"
-      submit-label="Войти"
+      :submit-label="submitLabel"
       @submit="onSubmit"
     >
       <FormKit
         v-model="email"
         type="email"
         label="Ваша почта"
+        :placeholder="emailPlaceholder"
       />
       <FormKit
         v-model="password"
@@ -43,10 +80,18 @@ async function onSubmit () {
       />
     </FormKit>
 
-    <p>
-      Нет аккаунта? <NuxtLink to="signup">
+    <p v-if="!isSignUpMode">
+      Нет аккаунта?
+      <ElButton type="primary" text @click="isSignUpMode = true">
         Зарегистрируйтесь
-      </NuxtLink>
+      </ElButton>
+    </p>
+
+    <p v-else>
+      Есть аккаунт?
+      <ElButton type="primary" text @click="isSignUpMode = false">
+        Войдите
+      </ElButton>
     </p>
   </div>
 </template>
